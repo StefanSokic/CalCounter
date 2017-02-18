2251b5759862983183021ab671e0ac11870f2971
Stefan Sokic <stefansokic@Stefans-MacBook-Pro.local>
2017-02-18 00:19:57 -0500

    modify spacing and variables

--- index.js
+++ index.js
@@ -7,64 +7,61 @@ app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
-//
-// // Process application/json
+
+// Process application/json
 app.use(bodyParser.json())
-//
-// // Index route
+
+// Index route
 app.get('/', function (req, res) {
 	res.send('Hello world, I am a chat bot')
-     })
-//
-//     // for Facebook verification
+})
+
+// for Facebook verification
 app.get('/webhook/', function (req, res) {
-         if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-                 res.send(req.query['hub.challenge'])
-                     }
-                         res.send('Error, wrong token')
-                         })
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    	res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
 
 // Spin up the server
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
 
-
 //Adding an API endpoint
 app.post('/webhook/', function (req, res) {
-	    messaging_events = req.body.entry[0].messaging
-		        for (i = 0; i < messaging_events.length; i++) {
-				        event = req.body.entry[0].messaging[i]
-						        sender = event.sender.id
-							        if (event.message && event.message.text) {
-									            text = event.message.text
-											                sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-													        }
-					    }
-	        res.sendStatus(200)
+	messaging_events = req.body.entry[0].messaging
+	for (i = 0; i < messaging_events.length; i++) {
+		event = req.body.entry[0].messaging[i]
+		sender = event.sender.id
+		if (event.message && event.message.text) {
+			text = event.message.text
+			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+		}
+	}
+	res.sendStatus(200)
 })
-
 var token = "EAAH4wv1b3eIBAC3Ld9Cg8ZCV0NVzEbiweExj4MI7THNoFpzMgW4WZCXrvPkohD4l9jf4uWs3hi6UfzyygK5VCZBYcizd0V1XSqvsvShhhLaJceMwGHTFGPttJDEohGR3peU6zDgiVkbGnY5WbT4j1a9U2soY1xZC1dJav8J7LgZDZD"
-
 
 //Adding function to echo back messages
 function sendTextMessage(sender, text) {
-	    messageData = {
-		            text:text
-				        }
-	        request({
-			        url: 'https://graph.facebook.com/v2.6/me/messages',
-				        qs: {access_token:token},
-					        method: 'POST',
-						        json: {
-								            recipient: {id:sender},
-									                message: messageData,
-											        }
-				    }, function(error, response, body) {
-					            if (error) {
-							                console.log('Error sending messages: ', error)
-										        } else if (response.body.error) {
-												            console.log('Error: ', response.body.error)
-														            }
-						        })
+	messageData = {
+		text:text
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}}, 
+		function(error, response, body) {
+			if (error) {
+				console.log('Error sending messages: ', error)
+			} else if (response.body.error) {
+				console.log('Error: ', response.body.error)
+			}
+	})
 }
