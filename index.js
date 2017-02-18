711614dffbbc4b6a8d1f20dcc2877d7c3b8018b4
Stefan Sokic <stefansokic@Stefans-MacBook-Pro.local>
2017-02-18 10:45:32 -0500

    checking webhook

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/webhook/', function (req, res) {
 		if (event.message && event.message.text) {
 			text = event.message.text
 			img = event.message.attachments[0].payload.url
-			sendTextMessage(sender, "you're fried" + img) //text.substring(0, 200))
+			sendTextMessage(sender, "you're fried" + text) //text.substring(0, 200))
 		}
 	}
 	res.sendStatus(200)
