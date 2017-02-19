2bea555c3c3b453bd3ac99c2defb1aeeada852e9
Stefan Sokic <stefansokic@Stefans-MacBook-Pro.local>
2017-02-19 00:44:28 -0500

    updating image recognition

--- test-clarifai.js
+++ test-clarifai.js
@@ -9,7 +9,7 @@ var app = new Clarifai.App(
 // predict the contents of an image by passing in a url
 app.models.predict(Clarifai.GENERAL_MODEL, 'https://d1qb2nb5cznatu.cloudfront.net/users/2093095-large?1482598409').then(
   function(response) {
-    console.log(response.outputs[0].data); //.outputs[0].data
+    console.log(response.outputs[0].data.concepts[0].name); //.outputs[0].data
   },
   function(err) {
     console.error(err);
